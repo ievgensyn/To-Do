76289359db6c34fdd4734d5ec7fa28a3f89f0d2e
Ievgen Synchyshyn <yevgens@web.de>
2018-01-29 15:09:15 +0200

    create the module app

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal To-Do tasks.',
+    'summary': '--',
+    'author': 'Ievgen Synchyshyn',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'depends': ['base'],
+    'application': True,
+}
