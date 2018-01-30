968bd21735c97a0215cc9e3c5f68004f8dc7961b
Ievgen Synchyshyn <yevgens@web.de>
2018-01-30 15:52:43 +0200

    add some buisiness logic layer(do_toggle_dane, do_clear_done)

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -6,7 +6,15 @@ from odoo.tests.common import TransactionCase
 class TestTodo(TransactionCase):
 
     def test_create(self):
-        "Create a simple t#odo"
+        "Create a simple todo"
         Todo = self.env['todo.task']
         task = Todo.create({'name': 'Test Task'})
         self.assertEqual(task.is_done, False)
+
+        # ...
+        # Test Toggle Done
+        task.do_toggle_done()
+        self.assertTrue(task.is_done)
+        # Test Clear Done
+        Todo.do_clear_done()
+        self.assertFalse(task.active)
