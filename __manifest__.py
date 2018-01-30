d782b89ec3b1beaedc6d97af45527974a0d5b520
Ievgen Synchyshyn <yevgens@web.de>
2018-01-30 17:24:07 +0200

    add access rules for groups -- test per user records rules is 'OK'

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,11 @@
     'version': '1.0',
     'category': 'Uncategorized',
     'depends': ['base'],
-    'data': ['views/todo_menu.xml',
-             'views/todo_view.xml'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/todo_access_rules.xml',
+        'views/todo_menu.xml',
+        'views/todo_view.xml',
+    ],
     'application': True,
 }
